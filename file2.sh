7156775e064651972e012627aea43d9156c18274
Kumbhari <703167263@ind.ge.ad>
2018-04-02 20:05:32 +0530

    Adding multiple files

---
+++ file2.sh
@@ -0,0 +1,1 @@
+What is “version control”, and why should you care? Version control is a system that records changes to a file or set of files over time so that you can recall specific versions later. For the examples in this book, you will use software source code as the files being version controlled, though in reality you can do this with nearly any type of file on a computer.
